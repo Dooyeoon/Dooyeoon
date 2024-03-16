2ffcf225cc2b7eb250f1002cf883b330c33d5a1d
Dooyeoon <determine99@naver.com>
2024-03-16 21:01:08 +0900

    Create time.py

---
+++ Implementation/time.py
@@ -0,0 +1,9 @@
+time = int(input())
+count = 0
+for h in range(time+1):
+    for m in range(60):
+        for s in range(60):
+            if '3' in str(h) + str(m) + str(s):
+                count += 1
+
+print(count)
